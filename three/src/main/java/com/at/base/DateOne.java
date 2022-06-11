c23559fdc2a38af9b290a516604fcef65d8ffa45
zabida <8888@qq.com>
2022-06-11 12:53:29 +0800

    升级2

--- three/src/main/java/com/at/base/DateOne.java
+++ three/src/main/java/com/at/base/DateOne.java
@@ -1,12 +1,63 @@
 package com.at.base;
 
-import java.time.DayOfWeek;
-import java.time.LocalDate;
+import org.apache.tomcat.jni.Local;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.*;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoUnit;
+import java.time.temporal.TemporalAdjuster;
+import java.time.temporal.TemporalAdjusters;
 import java.util.Date;
 
 public class DateOne {
-    public void plus(){
+    public void trans() throws ParseException {
+        // 时间对象转字符串
+        SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
+        String s = formatter.format(new Date());
+        // 字符串转对象
+        String dateStr = "2022-06-08";
+        SimpleDateFormat formatter2 = new SimpleDateFormat("yyyy-MM-dd");
+        formatter2.parse(dateStr);
+        // 一年多少天
+        SimpleDateFormat format = new SimpleDateFormat("D");
+        System.out.println(format.format(new Date()));  // 当前第多少天
+        // SimpleDateFormat线程不安全
 
+        //使用LocalDatetime
+        LocalDateTime time = LocalDateTime.of(2022, 6, 22, 11, 12, 3);
+        LocalDateTime now = LocalDateTime.now();
+        LocalDateTime time1 = LocalDateTime.of(LocalDate.now(), LocalTime.MIN);
+        LocalDateTime plus = time1.plus(1, ChronoUnit.DAYS);
+        // 指定本年当中第几天
+        LocalDateTime dayOfYear = now.withDayOfYear(5);
+        // 本月最后一天
+        LocalDateTime lastDayOfMonth = now.with(TemporalAdjusters.lastDayOfMonth());
+        Duration between = Duration.between(now, lastDayOfMonth);
+        long l = between.toDays(); // 相差天数
+        long until = now.until(lastDayOfMonth, ChronoUnit.DAYS);  // 计算间隔
+        // 格式化
+        DateTimeFormatter pattern = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+        String s1 = now.format(pattern);
+        String s2 = now.format(DateTimeFormatter.BASIC_ISO_DATE);
+        String s3 = now.format(DateTimeFormatter.ISO_LOCAL_DATE);
+        // 解析
+        LocalDateTime.parse(s1, pattern);
+        LocalDateTime.parse(s2, DateTimeFormatter.BASIC_ISO_DATE);
+        // 时间戳转换
+        long second = now.atZone(ZoneId.systemDefault()).toEpochSecond();
+        long milli = now.atZone(ZoneId.systemDefault()).toInstant().toEpochMilli(); // 毫秒
+        // 时间戳转LocalDateTime
+        LocalDateTime dateTime = LocalDateTime.ofInstant(Instant.ofEpochSecond(second), ZoneId.systemDefault());
+        LocalDateTime dateTime2 = LocalDateTime.ofInstant(Instant.ofEpochMilli(second), ZoneId.systemDefault());
+
+        //Date 转LocalDateTime
+        LocalDateTime localDateTime = new Date().toInstant().atZone(ZoneId.systemDefault()).toLocalDateTime();
+        Date date = Date.from(localDateTime.atZone(ZoneId.systemDefault()).toInstant());
+
+        // 比较先后
+        boolean before = lastDayOfMonth.isBefore(now);
     }
     public static void main(String[] args) {
         // LocalDate类 类似日历，可以提供很多功能
@@ -23,3 +74,4 @@ public class DateOne {
         LocalDate lastDay = now.plusMonths(1).minusDays(now.getDayOfMonth());
     }
 }
+
--- three/src/main/java/com/at/base/MathOne.java
+++ three/src/main/java/com/at/base/MathOne.java
@@ -1,8 +1,11 @@
 package com.at.base;
 
+import java.util.Map;
+import java.util.Random;
+
 public class MathOne {
     public static void main(String[] args) {
-        double random = Math.random();
+        double random = Math.random(); // [0, 1)
         System.out.println(random);
         float a = 334784620;
         System.out.println(a);
@@ -18,5 +21,32 @@ public class MathOne {
         String s = builder.toString();
         System.out.println(s);
 
+        double ceil = Math.ceil(3.2);  // 4.0
+        System.out.println(ceil);
+        double floor = Math.floor(3.2);  // 3.0
+        System.out.println(floor);
+        long round = Math.round(3.2);  // 3
+        long round1 = Math.round(3.8);  // 4
+        System.out.println(round + ":" + round1);
+        double pow = Math.pow(5, 3);// 5的3次方
+        System.out.println(pow);
+        double sqrt = Math.sqrt(64); // 8 开方
+        int abs = Math.abs(-45);  // 绝对值
+
+        Random rand = new Random();
+        //随机生成[0,1)之间的double类型的数据
+        System.out.println(rand.nextDouble());
+        //随机生成int类型允许范围之内的整型数据
+        System.out.println(rand.nextInt());
+        //随机生成[0,1)之间的float类型的数据
+        System.out.println(rand.nextFloat());
+        //随机生成false或者true
+        System.out.println(rand.nextBoolean());
+        //随机生成[0,10)之间的int类型的数据
+        System.out.print(rand.nextInt(10));
+        //随机生成[20,30)之间的int类型的数据
+        System.out.print(20 + rand.nextInt(10));
+        //随机生成[20,30)之间的int类型的数据（此种方法计算较为复杂）
+        System.out.print(20 + (int) (rand.nextDouble() * 10));
     }
 }
